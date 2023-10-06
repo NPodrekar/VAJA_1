58e2a0c1781f432592685050cef0834f375452ee
NPodrekar <np6718@student.uni-lj.si>
2023-10-06 16:34:46 +0200

    Domača naloga 1

--- file.py
+++ file.py
@@ -16,17 +16,41 @@ for number in [1,2,3,4,5,6,7,8]:
     print(number)
 """
 
-#funkcija ki vzame xyz in jih sesteje
-def sestevalnik(x,y,z):
-    rezultat = x+z+y
-    return rezultat
+###DOMAČA NALOGA1###
 
-print(sestevalnik(1,2,3))
+########### 1 ###########
+n = int(input('Vnesite katerokoli celo število: '))
+
+print("Spremenljivka je tipa ",type(n), "in ima vrednost " ,n)
+n=float(n)
+print("Spremenljivka je SEDAJ tipa ",type(n), "in ima vrednost " ,n)
+print("\n")
 
 
-#printej piramido
+########### 2 ###########
+x = [0,1,2,3,4,5,6,7,8,9]
+y = [9,8,7,6,5,4,3,2,1,0]
+z = []
 
-def piramida(visina):
+for i in range(len(x)):
+    z.append(x[i]+y[i])
+print(z)
+print("\n")
+
+
+########### 3 ###########
+num1 = input("Vnesite celoštevilsko vrednost: ")
+num2 = input("Vnesite še eno celoštevilsko vrednost: ")
+num3 = input("Vnesite še zadnjo celoštevilsko vrednost: ")
+print("Vnesli ste", num1, num2, "in", num3)
+test = lambda num1, num2, num3:  num1 == num2 , num1 >= num3
+print(test)
+print("\n")
+
+
+########### 4 ###########
+
+def izpisi_trikotnik(visina):
     for x in range(visina):
         while x >= 0:
 
@@ -34,4 +58,7 @@ def piramida(visina):
             x-=1
         print("")
 
-piramida(5)
+
+
+izpisi_trikotnik(5)
+print("\n")
